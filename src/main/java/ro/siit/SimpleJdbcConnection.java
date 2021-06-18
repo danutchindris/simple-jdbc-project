28e3a2a5cf48166e7b31722dd5883b62c87c4cdf
Danut Chindris <danutchindris@gmail.com>
2021-06-18 09:36:36 +0300

    Improve configuration; perform refactoring so that resources are managed in a try-with-resources block

--- src/main/java/ro/siit/SimpleJdbcConnection.java
+++ src/main/java/ro/siit/SimpleJdbcConnection.java
@@ -1,6 +1,5 @@
 package ro.siit;
 
-import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,22 +9,13 @@ import java.sql.SQLException;
 public class SimpleJdbcConnection {
 
     public static void main(String[] args) {
-        /*
-        // inainte de JDBC 4, era nevoie de:
-        try {
-            // FQN - fully qualified name
-            Class.forName("org.postgresql.Driver").getDeclaredConstructor().newInstance();
-        } catch (InstantiationException | IllegalAccessException
-                | ClassNotFoundException | NoSuchMethodException | InvocationTargetException e) {
-            System.err.println("Can't load driver. Verify CLASSPATH");
-            System.err.println(e.getMessage());
-        }
-        */
 
-        try (Connection conn = DriverManager
-                .getConnection("jdbc:postgresql://localhost:5432/company?user=postgres&password=postgres")) {
-            PreparedStatement ps = conn.prepareStatement("select first_name from employees");
-            ResultSet rs = ps.executeQuery();
+        try (
+                Connection conn = DriverManager
+                .getConnection("jdbc:postgresql://localhost:5432/company?user=postgres&password=postgres");
+             PreparedStatement ps = conn.prepareStatement("select first_name from employees");
+             ResultSet rs = ps.executeQuery()
+        ) {
             while (rs.next()) {
                 System.out.println(rs.getString(1));
             }
